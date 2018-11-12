e16d9273ec3a015035eba0257422f5999619a039
Colin <cholin@gmail.com>
2018-11-12 00:16:43 -0500

    Reduce size of original images to max 1280px for faster web loading

--- bin/generate-thumbnails.bat
+++ bin/generate-thumbnails.bat
@@ -4,14 +4,14 @@ REM First, unzip a portable copy of ImageMagick into /ImageMagick
 REM All files can be deleted except mogrify.exe (and the license)
 
 REM The following commands are run against each subdirectory of "..\assets\*"
-REM     - mogrify -strip '*.*'
-REM         - Removes all data from the source images
+REM     - mogrify -strip -resize "1280>x1280>" '*.*'
+REM         - Removes all data from the source images and resizes to max 1280px
 REM     - mkdir "thumbs"
 REM     - mogrify -path thumbs -thumbnail 250x250 '*.*'
 REM         - Creates thumbnails up to a maximum height of 250px
 
 FOR /D %%d IN ("..\assets\*") DO (
-    "ImageMagick\mogrify.exe" -strip "%%d\*.*"
+    "ImageMagick\mogrify.exe" -strip -resize "1280>x1280>" "%%d\*.*"
     mkdir "%%d\thumbs"
     "ImageMagick\mogrify.exe" -path "%%d\thumbs" -thumbnail 250x250 "%%d\*.*"
 )
